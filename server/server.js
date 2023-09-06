2b43c757f209c86e9cb3f5d57d7128262e4b23bf
Moiz-Ali-Khan <moizalikhan215@gmail.com>
2023-09-07 02:51:35 +0500

    routes completed

--- server/server.js
+++ server/server.js
@@ -35,12 +35,41 @@ app.get('/api/v1/restaurants',(req,res) => {
 //Get a individual restaurant
 app.get('/api/v1/restaurants/:id',(req,res) => {
     console.log(req.params);
+
+    res.status(200).json({
+        status: "success",
+        data:{
+            restaurant: "chai dewari"
+        }
+    })
 });
 
 //Create a restaurant
 app.post('/api/v1/restaurants/',(req,res) => {
     console.log(req.body);
+    res.status(201).json({
+        status: "success",
+        data:{
+            restaurant: "chai dewari"
+        }
+    })
 });
 
 //Update a restaurant
+app.put('/api/v1/restaurants/:id',(req,res) => {
+    console.log(req.body);
+    res.status(200).json({
+        status: "success",
+        data:{
+            restaurant: "chai dewari"
+        }
+    })
+});
 
+//Delete a restaurant
+app.delete('/api/v1/restaurants/:id',(req,res) => {
+    console.log(req.body);
+    res.status(204).json({
+        status: "success",
+    })
+});
