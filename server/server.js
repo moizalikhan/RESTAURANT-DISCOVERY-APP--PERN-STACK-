96255743ccd4eec20c067af2bec0d4a1e24c2ef1
Moiz-Ali-Khan <moizalikhan215@gmail.com>
2023-09-08 16:30:22 +0500

    changes

--- server/server.js
+++ server/server.js
@@ -37,47 +37,75 @@ app.get('/api/v1/restaurants',async(req,res) => {
             restaurants:results.rows,
         },
 });
-} catch(err){console.log(err);}    
+} catch(err){console.log(err);}
 });
 
 
 
 //Get a individual restaurant
-app.get('/api/v1/restaurants/:id',(req,res) => {
+app.get('/api/v1/restaurants/:id',async(req,res) => {
     console.log(req.params.id);
 
-    res.status(200).json({
-        status: "success",
-        data:{
-            restaurant: "chai dewari"
-        }
-    })
+    try
+    {
+        const results= await db.query(
+        "select * from restaurants where id = $1",[req.params.id]
+        );
+        res.status(200).json({
+            status: "success",
+            data:{
+                restaurant:results.rows[0],
+            }
+        })
+    }
+    catch(err)
+    {
+        console.log(err);
+    }
 });
 
 
 
 //Create a restaurant
-app.post('/api/v1/restaurants/',(req,res) => {
+app.post('/api/v1/restaurants/',async(req,res) => {
     console.log(req.body);
-    res.status(201).json({
-        status: "success",
-        data:{
-            restaurant: "chai dewari"
-        }
-    })
+    try{
+        const results = await db.query(
+        "insert into restaurants(Restaurant_name,location,price_rangeofRestaurants) values ($1,$2,$3) returning *",
+        [req.body.Restaurant_name,req.body.location,req.body.price_rangeofRestaurants]
+        )
+            console.log(results);
+        res.status(201).json({
+            status: "success",
+            data:{
+                restaurant:results.rows[0],
+            },
+        });
+    }
+    catch(err){
+        console.log(err);
+    }
 });
 
 
 
 //Update a restaurant
-app.put('/api/v1/restaurants/:id',(req,res) => {
+app.put('/api/v1/restaurants/:id',async (req,res) => {
     console.log(req.body);
-    res.status(200).json({
-        status: "success",
-        data:{
-            restaurant: "chai dewari"
-        }
-    })
+    try{
+        const results = await db.query(
+        "update restaurants set Restaurant_name = $1, location = $2, price_rangeofRestaurants = $3 where id = $4 returning *",
+        [req.body.Restaurant_name, req.body.location, req.body.price_rangeofRestaurants, req.params.id])
+        res.status(200).json({
+            status: "success",
+            data:{
+                restaurant: results.rows[0],
+            },
+        });        
+    }
+    catch(err){
+        console.log(err);
+    }   
 });
 
 
